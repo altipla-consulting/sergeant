4371634899417b8a1e2387f63faad1b63168acc1
Ernesto Alejo <ernestokarim@gmail.com>
2023-07-04 20:59:44 +0200

    Fix the name of the GKE auth plugin needed for CLI operations.

--- files/install.sh
+++ files/install.sh
@@ -183,14 +183,14 @@ fi
 # Install: Gcloud
 if ! command -v gcloud &> /dev/null
 then
-  echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] http://packages.cloud.google.com/apt cloud-sdk main" | sudo tee /etc/apt/sources.list.d/google-cloud-sdk.list
+  echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
   curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key --keyring /usr/share/keyrings/cloud.google.gpg add -
   sudo apt update
   sudo apt install -y google-cloud-sdk kubectl
 fi
 if ! command -v gke-gcloud-auth-plugin &> /dev/null
 then
-  sudo apt -y install google-cloud-sdk-gke-gcloud-auth-plugin
+  sudo apt -y install google-cloud-cli-gke-gcloud-auth-plugin
 fi
 gcloud --quiet auth configure-docker europe-west1-docker.pkg.dev,eu.gcr.io,gcr.io
 
