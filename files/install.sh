967ee1b7bbe00108fe7c29915532ae476ce91c55
Ernesto Alejo <ernestokarim@gmail.com>
2024-03-08 23:15:52 +0100

    Install private Go libraries from GitHub.

--- files/install.sh
+++ files/install.sh
@@ -172,7 +172,7 @@ ssh-keyscan -p 29418 gerrit.altipla.consulting >> ~/.ssh/known_hosts
 
 # Install: Go private packages.
 git config --global url."ssh://git@github.com:".insteadOf "https://github.com"
-/usr/local/go/bin/go env -w GOPRIVATE=github.com/lavozdealmeria,github.com/altipla-consulting,go.buf.build,buf.build,gerrit.altipla.consulting
+/usr/local/go/bin/go env -w GOPRIVATE=github.com/lavozdealmeria,github.com/altipla-consulting,github.com/altec-informatica,go.buf.build,buf.build,gerrit.altipla.consulting
 
 # Install: Altipla tools.
 /usr/local/go/bin/go install github.com/altipla-consulting/gendc@latest
